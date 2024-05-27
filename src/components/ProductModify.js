5bce892a12ec538b75ba084b65dc823b66adfbd4
rnjs9861 <rkwkvlrmakfh@gmail.com>
2024-05-27 14:13:51 +0900

    5/27 수정기능 작동

--- src/components/ProductModify.js
+++ src/components/ProductModify.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { getProductOne } from "../api/productItemApi";
-import { deleteProduct, modifyProduct } from "../api/productModifyApi";
-import axios from "axios";
-import Modal from "./common/Modal";
+import {
+  deleteProduct,
+  getProductOne,
+  modifyProduct,
+} from "../api/productModifyApi";
 import useModal from "../hooks/useModal";
 import Button from "./common/Button";
-import styled from "@emotion/styled";
+import Modal from "./common/Modal";
 
 const initState = {
   id: "",
@@ -15,19 +17,18 @@ const initState = {
   explanation: "",
 };
 
-const ProductModify = () => {
+const Modify = () => {
   const navigate = useNavigate();
-  const { productId } = useParams();
 
+  const { productId } = useParams();
   // useModal 커스텀 훅을 사용하여 모달 상태와 제어 함수 가져오기
   const { isModalOpen, modalMessage, confirmAction, openModal, closeModal } =
     useModal();
 
   const [product, setProduct] = useState(initState);
-
   const [isLoading, setIsLoading] = useState(false);
 
-  // 입력값이 변경될 때 상태를 업데이트 하는 함수
+  // 입력값이 변경될 때 상태를 업데이트하는 함수
   const handleChange = e => {
     const updateProduct = { ...product, [e.target.name]: e.target.value };
     setProduct(updateProduct);
@@ -37,37 +38,36 @@ const ProductModify = () => {
   const handlePatchProduct = async product => {
     try {
       await modifyProduct(product);
-
-      console.log("수정완료");
-      //수정 완료 후 모달을 띄움
+      console.log("수정 완료");
+      // 수정 완료 후 모달을 띄움
       openModal({
-        message: "수정이 완료 되었습니다!",
+        message: "수정이 완료되었습니다!",
         onConfirm: () => {
           closeModal();
           navigate(`/product/${productId}`);
         },
       });
     } catch (error) {
-      console.log("수정 중 에러 발생", error);
+      console.log("수정 중 오류 발생", error);
     }
   };
 
-  // 폼 제출시 호출되는 함수
+  // 폼 제출 시 호출되는 함수
   const handleSubmit = async e => {
     e.preventDefault();
-
     await handlePatchProduct(product);
   };
 
   // 상품 삭제 확인 모달을 여는 함수
-  const handleDelete = async id => {
+  const handleDelete = () => {
     openModal({
       message: "정말 삭제하시겠습니까?",
       onConfirm: async () => {
         try {
           await deleteProduct(productId);
+          console.log("삭제 완료");
           closeModal();
-          navigate(`/product/`);
+          navigate("/product");
         } catch (error) {
           console.log("삭제 중 오류 발생", error);
         }
@@ -75,7 +75,7 @@ const ProductModify = () => {
     });
   };
 
-  //상품 정보를 가져오는 함수
+  // 상품 정보를 가져오는 함수
   const fetchProduct = async id => {
     setIsLoading(true);
 
@@ -84,9 +84,9 @@ const ProductModify = () => {
       const data = response.data;
       if (data && data.product) {
         setProduct(data.product);
+        console.log("상품 조회 완료");
       } else {
         console.log("상품 불러오는데 실패");
-        return;
       }
     } catch (error) {
       console.log("상품 조회 중 오류 발생", error);
@@ -99,16 +99,14 @@ const ProductModify = () => {
     fetchProduct(productId);
   }, [productId]);
 
-  console.log(product.id);
-
   if (isLoading) {
-    return <h3>상품 정보를 불러오는 중입니다.</h3>;
+    return <h3>상품 정보를 불러오는 중입니다...</h3>;
   }
 
   return (
     <div>
       <h2>상품 수정하기</h2>
-      <Button label="상품 수정하기" onSubmit={handleSubmit} autoComplete="off">
+      <form onSubmit={handleSubmit} autoComplete="off">
         <input
           type="text"
           name="name"
@@ -132,17 +130,9 @@ const ProductModify = () => {
         />
         <br />
         <Button label="상품 수정하기" />
-      </Button>
-      <Button
-        label="상품 삭제하기"
-        onClick={() => {
-          handleDelete();
-          console.log("삭제완료");
-        }}
-      >
-        상품 삭제하기
-      </Button>
-
+      </form>
+      <Button label="상품 삭제하기" onClick={handleDelete} />
+      {/* 모달 관련 */}
       <Modal
         isOpen={isModalOpen}
         message={modalMessage}
@@ -153,4 +143,4 @@ const ProductModify = () => {
   );
 };
 
-export default ProductModify;
+export default Modify;
